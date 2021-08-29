570d83f54251874d049f09ed9edf1b3caf1a072f
loukel <Louis.Kelly1@outlook.com>
2021-08-29 10:51:26 +0100

    Added basic column functions

--- controllers/columnController.js
+++ controllers/columnController.js
@@ -1,7 +1,51 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+const errorHandler = async (callback) => {
+  return () => {
+    try {
+      return await callback()
+    } catch (error) {
+      console.error(error)
+      res.sendStatus(500)
+    }
+  }
+}
 
+const get_columns = errorHandler(async (req, res) => {
+  const columns = await prisma.column.findMany()
+  res.status(200).send(columns)
+})
+
+const update_columns = errorHandler(async (req, res) => {
+  const data = req.body
+  let queries = []
+  data.forEach(update => {
+    const id = update.id
+
+    const data = {
+      nextId: update.nextId,
+    }
+
+    const query = prisma.state.updateMany({
+      where: {
+        id
+      },
+      data
+    })
+
+    queries.push(query)
+  })
+  await prisma.$transaction(queries)
+})
+
+const update_column = errorHandler(async (req, res) => {
+  const data = req.body
+  await prisma.column.update({
+    data
+  })
+  res.sendStatus(204)
+})
 
 module.exports = {
   get_columns,
